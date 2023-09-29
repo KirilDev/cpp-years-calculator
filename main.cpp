640421f9d52a7fa9642eb1850ae593c48bdc127c
Kirill_Dev <126055944+KirilDev@users.noreply.github.com>
2023-09-29 15:56:45 +0300

    Upload program
    upload program files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int bornData;
+    int currentYear;
+
+    std::cout << "At the first, enter current year: ";
+    std::cin >> currentYear;
+    std::cout << "And in the second, enter the date when you born: ";
+    std::cin >> bornData;
+    std::cout << "Your age is " << currentYear - bornData;
+    return 0;
+}
