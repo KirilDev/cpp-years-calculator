3718c2103dbcaddda3835e80c77833b62d29664a
Kirill_Dev <126055944+KirilDev@users.noreply.github.com>
2024-01-02 19:23:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+int main(void) {
     int bornData;
     int currentYear;
 
@@ -9,5 +9,6 @@ int main() {
     std::cout << "And in the second, enter the date when you born: ";
     std::cin >> bornData;
     std::cout << "Your age is " << currentYear - bornData;
+    
     return 0;
 }
